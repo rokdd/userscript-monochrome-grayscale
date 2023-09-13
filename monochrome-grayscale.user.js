fe9316e758d30b626106426aec413bc28e31d0f0
rokdd <640866+rokdd@users.noreply.github.com>
2023-09-13 11:43:08 +0200

    Update monochrome-grayscale.user.js

--- monochrome-grayscale.user.js
+++ monochrome-grayscale.user.js
@@ -1,11 +1,17 @@
 // ==UserScript==
 // @name         Monochrome grayscale website
 // @namespace    https://github.com/rokdd/userscript-monochrome-grayscale/
-// @version      0.1
+// @homepage     https://github.com/rokdd/userscript-monochrome-grayscale/
+// @version      0.2
 // @description  Display websites in grayscale
 // @author       rokdd
 // @match        https://web.whatsapp.com/*
+// @match        http://*/*
+// @match        https://*/*
 // @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @updateURL    https://github.com/rokdd/userscript-monochrome-grayscale/raw/main/monochrome-grayscale.user.js
 // @downloadURL  https://github.com/rokdd/userscript-monochrome-grayscale/raw/main/monochrome-grayscale.user.js
@@ -15,6 +21,9 @@
     // @match        http://*/*
 
     'use strict';
+    var USERDEFINED_MATCHING=true;
+function hello() { alert("hello"); }
+GM.registerMenuCommand("Hello, world!", hello, "h");
     GM_addStyle ( `
    html {
     -moz-filter: grayscale(100%) !important;
